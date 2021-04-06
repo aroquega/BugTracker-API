8c754bc7dbcec1d4e27b476006498beb19555246
Hristo Roque <aroquega@unsa.edu.pe>
2021-04-06 17:19:29 -0500

    Create Db Context and configure IssueLabel join table

---
+++ Models/BugTrackerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Models
+{
+    public class BugTrackerContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<IssueLabel> IssueLabels { get; set; }
+        
+        public BugTrackerContext(DbContextOptions<BugTrackerContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IssueLabel>().HasKey(il => new
+            {
+                il.IssueId,
+                il.LabelId
+            });
+        }
+    }
+}
